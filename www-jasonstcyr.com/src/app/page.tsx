b364e701c3fa0e59b9db32bf471f3728a15aba6c
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-03-13 12:01:29 -0400

    Updating layout to have image column

--- www-jasonstcyr.com/src/app/page.tsx
+++ www-jasonstcyr.com/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { type SanityDocument } from "next-sanity";
+import Image from "next/image";
 
 import { client } from "@/sanity/client";
 
@@ -14,14 +15,19 @@ export default async function IndexPage() {
   const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
 
   return (
-    <main className="container mx-auto min-h-screen max-w-3xl p-8">
+    <main className="container mx-auto min-h-screen max-w-4xl p-8">
       <h1 className="text-4xl font-bold mb-8">Posts</h1>
-      <div className="grid grid-cols-1 gap-y-4">
+      <div className="grid grid-cols-1 gap-y-6">
         {posts.map((post) => (
           <div className="p-4 border rounded-lg shadow-md" key={post._id}>
-            <Link href={`/${post.slug.current}`}>
-              <h2 className="text-xl font-semibold">{post.title}</h2>
-              <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
+            <Link href={`/${post.slug.current}`} className="flex gap-6">
+              <div className="w-1/4 flex-shrink-0">
+                {"TBD"}
+              </div>
+              <div className="w-3/4">
+                <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
+                <p className="text-gray-600">{new Date(post.publishedAt).toLocaleDateString()}</p>
+              </div>
             </Link>
           </div>
         ))}
