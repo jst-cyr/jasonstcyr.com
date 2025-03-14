32d435e28d9c15db5fd5b6a134fa8a53c5cd3d21
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-03-14 12:05:35 -0400

    Adding new query to get posts by a tag

--- www-jasonstcyr.com/src/queries/posts.ts
+++ www-jasonstcyr.com/src/queries/posts.ts
@@ -3,6 +3,30 @@ import { PortableTextBlock, SanityDocument } from "next-sanity";
 
 export const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
 
+export const POSTS_BY_TAG_QUERY = `*[
+  _type == "post"
+  && defined(slug.current)
+  && $tag in tags[]
+]|order(publishedAt desc)[0...12]{
+  _id, 
+  title, 
+  slug, 
+  publishedAt,
+  image {
+    asset->{
+      _id,
+      url,
+      metadata {
+        dimensions {
+          width,
+          height
+        }
+      }
+    }
+  },
+  "summary": body[0].children[0].text
+}`;
+
 export const POSTS_QUERY = `*[
   _type == "post"
   && defined(slug.current)
