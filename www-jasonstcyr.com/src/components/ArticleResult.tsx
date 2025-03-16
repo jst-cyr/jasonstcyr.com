5941df2be03660cb7dced8c7263457fa1d0341ca
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-03-16 17:48:29 -0400

    Adding new component to handle each individual article display
    Future goal to use with search results and listings

---
+++ www-jasonstcyr.com/src/components/ArticleResult.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import Image from "next/image";
+import { type SanityDocument } from "next-sanity";
+
+interface ArticleResultProps {
+  id: string;
+  slug: string;
+  title: string;
+  summary: string;
+  publishedAt: string;
+  image: {
+    asset: {
+      url: string;
+    };
+  };
+}
+
+const ArticleResult: React.FC<ArticleResultProps> = ({ id, slug, title, summary, publishedAt, image }) => {
+  return (
+        <div className="p-4 border rounded-lg shadow-md" key={id}>
+          <Link href={`/${slug}`} className="flex gap-6">
+            <div className="w-1/4 flex-shrink-0">
+              {image?.asset && (
+                <Image
+                  src={image.asset.url}
+                  alt={title}
+                  width={200}
+                  height={150}
+                  className="rounded-md object-cover w-full h-32"
+                />
+              )}
+            </div>
+            <div className="w-3/4">
+              <h2 className="text-xl font-semibold mb-2">{title}</h2>
+              <p className="text-gray-400">{new Date(publishedAt).toLocaleDateString()}</p>
+              {summary && (
+                <p className="text-gray-400">{summary.slice(0, 200)}...</p>
+              )}
+              <p className="mt-2 text-sm">Read more</p>
+            </div>
+          </Link>
+        </div>
+  );
+};
+
+export default ArticleResult;
--- www-jasonstcyr.com/src/components/PostList.tsx
+++ www-jasonstcyr.com/src/components/PostList.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { type SanityDocument } from "next-sanity";
+import ArticleResult from "./ArticleResult";
 
 interface PostListProps {
   posts: SanityDocument[];
@@ -10,29 +11,15 @@ const PostList: React.FC<PostListProps> = ({ posts }) => {
   return (
     <div className="grid grid-cols-1 gap-y-6">
       {posts.map((post) => (
-        <div className="p-4 border rounded-lg shadow-md" key={post._id}>
-          <Link href={`/${post.slug.current}`} className="flex gap-6">
-            <div className="w-1/4 flex-shrink-0">
-              {post.image?.asset && (
-                <Image
-                  src={post.image.asset.url}
-                  alt={post.title}
-                  width={200}
-                  height={150}
-                  className="rounded-md object-cover w-full h-32"
-                />
-              )}
-            </div>
-            <div className="w-3/4">
-              <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
-              <p className="text-gray-400">{new Date(post.publishedAt).toLocaleDateString()}</p>
-              {post.summary && (
-                <p className="text-gray-400">{post.summary.slice(0, 200)}...</p>
-              )}
-              <p className="mt-2 text-sm">Read more</p>
-            </div>
-          </Link>
-        </div>
+        <ArticleResult 
+          key={post._id}
+          id={post._id}
+          slug={post.slug.current}
+          title={post.title}
+          summary={post.summary}
+          publishedAt={post.publishedAt}
+          image={post.image}
+        />
       ))}
     </div>
   );
