a612995c2035a95b1775ba74ac39c1da5b6c457c
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-03-15 22:38:20 -0400

    Updating to use NEXT_PUBLIC_ versions of environment variables

--- www-jasonstcyr.com/src/components/Search.tsx
+++ www-jasonstcyr.com/src/components/Search.tsx
@@ -3,9 +3,9 @@ import { liteClient as algoliasearch } from 'algoliasearch/lite';
 import { SearchBox } from 'react-instantsearch';
 import { InstantSearchNext } from 'react-instantsearch-nextjs';
 
-const algoliaAppId = process.env.ALGOLIA_APP_ID!;
-const algoliaApiKey = process.env.ALGOLIA_SEARCH_API_KEY!;
-const indexName = process.env.ALGOLIA_INDEX_NAME!;
+const algoliaAppId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!;
+const algoliaApiKey = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY!;
+const indexName = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME!;
 const searchClient = algoliasearch(algoliaAppId, algoliaApiKey);
 
 export function Search() {
