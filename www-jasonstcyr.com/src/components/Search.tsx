58059e81364bea972f9e6df970f8b8f09389f21f
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-03-16 17:28:21 -0400

    Adding some basic configuration for SearchBox and useState

--- www-jasonstcyr.com/src/components/Search.tsx
+++ www-jasonstcyr.com/src/components/Search.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { liteClient as algoliasearch } from 'algoliasearch/lite';
-import { SearchBox } from 'react-instantsearch';
+import { SearchBox, Hits } from 'react-instantsearch';
 import { InstantSearchNext } from 'react-instantsearch-nextjs';
-import { history } from 'instantsearch.js/es/lib/routers';
+import { useState } from 'react';
 
 const algoliaAppId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!;
 const algoliaApiKey = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY!;
@@ -10,21 +10,30 @@ const indexName = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME!;
 const searchClient = algoliasearch(algoliaAppId, algoliaApiKey);
 
 export function Search() {
-  return (
-    <InstantSearchNext 
-        searchClient={searchClient} 
-        indexName={indexName}
-        initialUiState={{
-            [indexName]: { query: '' },
-        }}
-        routing={true}
-        future={{
-            preserveSharedStateOnUnmount: true,
-        }}
-        
-    >
-      <SearchBox />
-    </InstantSearchNext>
-  );
+    const [results, setResults] = useState('');
+    
+    return (
+        <InstantSearchNext 
+            searchClient={searchClient} 
+            indexName={indexName}
+            initialUiState={{
+                [indexName]: { query: '' },
+            }}
+            routing={true}
+            future={{
+                preserveSharedStateOnUnmount: true,
+            }}
+            
+        >
+        <SearchBox
+            placeholder="Search for articles..."
+            classNames={{
+                input: 'bg-gray-800 p-4 rounded-lg',
+                submit:'hidden',
+                reset: 'hidden',
+            }}
+        />
+        </InstantSearchNext>
+    );
 }
 
