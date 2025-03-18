a1f63cd89e338f37ff56bf3e83cacf9045e54348
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-03-18 14:48:24 -0400

    Adding two carousels to the home page

--- www-jasonstcyr.com/src/app/page.tsx
+++ www-jasonstcyr.com/src/app/page.tsx
@@ -4,6 +4,7 @@ import { POSTS_QUERY } from '../queries/posts';
 import PostList from "@/components/PostList";
 import { PostData } from "@/types/post";
 import TagPostList from "@/components/TagPostList";
+
 const options = { next: { revalidate: 30 } };
 
 export default async function IndexPage() {
@@ -21,11 +22,11 @@ export default async function IndexPage() {
 
   return (
     <main className="container mx-auto min-h-screen max-w-4xl p-8">
-      <h1 className="text-4xl font-bold mb-8">Jason St. Cyr</h1>
+      <h1 className="text-4xl font-bold mb-8">Jason St-Cyr</h1>
       <h2 className="text-2xl font-bold mb-4 mt-8">Sitecore</h2>
-      <TagPostList tag="sitecore" />
+      <TagPostList tag="sitecore" displayMode="carousel" />
       <h2 className="text-2xl font-bold mb-4 mt-8">Lani</h2>
-      <TagPostList tag="lani" />
+      <TagPostList tag="lani" displayMode="carousel" />
       <h2 className="text-2xl font-bold mb-4 mt-8">Recent Posts</h2>
       <PostList posts={posts} containerId="recent-posts" />
     </main>
