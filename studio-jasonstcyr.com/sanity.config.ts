ae447ce129d90a12b83095cdefbb73b232d615c2
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-03-24 12:24:51 -0400

    WP Import: Adding codeInput() to the Sanity config

--- studio-jasonstcyr.com/sanity.config.ts
+++ studio-jasonstcyr.com/sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {media} from 'sanity-plugin-media'
+import {codeInput} from '@sanity/code-input'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
@@ -11,7 +12,7 @@ export default defineConfig({
   projectId: 'sswab2yy',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool(), media()],
+  plugins: [structureTool(), visionTool(), media(), codeInput()],
 
   schema: {
     types: schemaTypes,
