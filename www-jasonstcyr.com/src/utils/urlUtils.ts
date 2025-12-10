a23809f7d3bdb46f8b581263fdf58146fc3d86eb
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2025-12-09 22:09:56 -0500

    Fixing deprecated imageUrlBuilder

--- www-jasonstcyr.com/src/utils/urlUtils.ts
+++ www-jasonstcyr.com/src/utils/urlUtils.ts
@@ -1,10 +1,12 @@
-import imageUrlBuilder from "@sanity/image-url";
+import { createImageUrlBuilder } from "@sanity/image-url";
 import type { SanityImageSource } from "@sanity/image-url";
 import { client } from "@/sanity/client"; 
 
 const { projectId, dataset } = client.config();
 
+const builder = projectId && dataset 
+  ? createImageUrlBuilder({ projectId, dataset })
+  : null;
+
 export const urlFor = (source: SanityImageSource) =>
-  projectId && dataset
-    ? imageUrlBuilder({ projectId, dataset }).image(source)
-    : null;
+  builder ? builder.image(source) : null;
